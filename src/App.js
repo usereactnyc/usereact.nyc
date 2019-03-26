496b410883624c2476717b50c920a2287d23204a
donavon <github@donavon.com>
2019-03-26 10:43:55 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Welcome to the future home of{' '}
+        <a
+          className="App-link"
+          href="https://twitter.com/useReactNYC"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @useReactNYC
+        </a>
+      </p>
+    </header>
+  </div>
+);
 
 export default App;
