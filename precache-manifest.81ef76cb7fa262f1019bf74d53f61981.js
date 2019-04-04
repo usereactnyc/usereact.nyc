c4a888b0390e2952fb4b23134976d49e51490114
Matthew Gerstman <mgerstman@dropbox.com>
2019-04-04 14:53:54 -0400

    Updates

---
+++ precache-manifest.81ef76cb7fa262f1019bf74d53f61981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f873db443ab451a4abd83aa8c979293b",
+    "url": "/static/media/logo.f873db44.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3995859b737cfddb5f87",
+    "url": "/static/js/main.fa82445f.chunk.js"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "3995859b737cfddb5f87",
+    "url": "/static/css/main.3527abd3.chunk.css"
+  },
+  {
+    "revision": "a57b1f10ee44d4cbea4cbfd6f174f40a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2aa28ff7e7b7a4da46f3b4776d263394.js"
+  "/precache-manifest.81ef76cb7fa262f1019bf74d53f61981.js"
 );
 
 workbox.clientsClaim();
