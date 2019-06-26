ba06068f9546998ba7131604f59480231351cc7d
Matthew Gerstman <matthew.gerstman@gmail.com>
2019-06-26 09:48:12 -0400

    Clean up organizers code (#46)
    * Shuffle organizers
    
    * Add alt tag
    
    * Switch to twitter as key
    
    * Only shuffle organizers once per build
    
    * Try this with a hook
    
    * use image as key
    
    * Don't shuffle organizers
    
    * Remove unused import

--- src/pages/home/Organizers.tsx
+++ src/pages/home/Organizers.tsx
@@ -1,10 +1,38 @@
 import * as React from "react";
-import donavon2 from "../../assets/donavon@2x.png";
-import harry2 from "../../assets/harry@2x.png";
-import jenn2 from "../../assets/jenn@2x.png";
-import matthew2 from "../../assets/matthew@2x.png";
+import donavon from "../../assets/donavon@2x.png";
+import harry from "../../assets/harry@2x.png";
+import jenn from "../../assets/jenn@2x.png";
+import matthew from "../../assets/matthew@2x.png";
 import Section from "../../components/Section";
 import TextGroup from "../../components/TextGroup";
+
+type Organizer = {
+  twitter: string;
+  image: string;
+  name: string;
+};
+const Organizers: Organizer[] = [
+  {
+    image: donavon,
+    name: "Donavon West",
+    twitter: "donavon"
+  },
+  {
+    image: harry,
+    name: "Harry Wolff",
+    twitter: "hswolff"
+  },
+  {
+    image: jenn,
+    name: "Jenn Creighton",
+    twitter: "gurlcode"
+  },
+  {
+    image: matthew,
+    name: "Matthew Gerstman",
+    twitter: "MatthewGerstman"
+  }
+];
 
 export default () => {
   return (
@@ -13,52 +41,27 @@ export default () => {
         <h2 className="centered-text">The Organizers</h2>
       </TextGroup>
       <ul className="organizers-ul">
-        <li>
-          <a
-            href="https://twitter.com/donavon"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <div>
-              <img src={donavon2} />
-              <p>Donavon West</p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/hswolff"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <div>
-              <img src={harry2} /> <p>Harry Wolff</p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/gurlcode"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <div>
-              <img src={jenn2} /> <p>Jenn Creighton</p>
-            </div>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/MatthewGerstman"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <div>
-              <img src={matthew2} /> <p>Matthew Gerstman</p>
-            </div>
-          </a>
-        </li>
+        {Organizers.map((organizer) => (
+          <Organizer {...organizer} key={organizer.image} />
+        ))}
       </ul>
     </Section>
   );
 };
+
+function Organizer({ name, image, twitter }: Organizer) {
+  return (
+    <li>
+      <a
+        href={`https://twitter.com/${twitter}`}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <div>
+          <img src={image} alt={`Photo of ${name}`} />
+          <p>{name}</p>
+        </div>
+      </a>
+    </li>
+  );
+}
