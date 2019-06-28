76ac3325d978457f3b6d1f9d467d7f747d26335b
Sung M. Kim <sungmkim80@gmail.com>
2019-06-27 21:06:10 -0400

    Fixed 'Zooming and scaling must not be disabled' by changing the 'maximum-scale' value from 1.0 to 5.0, which is the recommended by Axe

--- src/html.tsx
+++ src/html.tsx
@@ -33,7 +33,7 @@ export default (props: HtmlProps) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=5.0"
         />
         {head.title.toComponent()}
         {head.meta.toComponent()}
