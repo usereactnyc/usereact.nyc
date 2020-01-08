0d17a9b1c7add1ffc3363df6b612518a65cb4fe4
Matthew Gerstman <matthew.gerstman@gmail.com>
2020-01-07 20:05:42 -0500

    Add policy for minors (#67)
    * Add policy for minors
    
    * Comma
    
    * I can't spell, but Nick can

---
+++ src/pages/home/Minors.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import TextGroup from "../../components/TextGroup";
+
+export default () => {
+  return (
+    <React.Fragment>
+      <section className="minors-container" id="minors">
+        <h2>A note for our younger attendees</h2>
+        <TextGroup>
+          <p>
+            If you're under the age of 18 you're welcome at our events, however
+            we require that you have a chaperone 21 or older. If that person is
+            not your parent/guardian, please have your parent/guardian email us
+            at{" "}
+            <a href="mailto:organizers@usereact.nyc">organizers@usereact.nyc</a>{" "}
+            in advance with their name.
+          </p>
+        </TextGroup>
+      </section>
+    </React.Fragment>
+  );
+};
