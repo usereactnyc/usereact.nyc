23efb612471a9b3854d591e140da22d62e2282ef
donavon <github@donavon.com>
2020-01-13 14:51:27 -0500

    fix grammar

--- src/pages/home/Minors.tsx
+++ src/pages/home/Minors.tsx
@@ -8,7 +8,7 @@ export default () => {
         <h2>A note for our younger attendees</h2>
         <TextGroup>
           <p>
-            If you're under the age of 18 you're welcome at our events, however
+            If you're under the age of 18, you're welcome at our events. However,
             we require that you have a chaperone 21 or older. If that person is
             not your parent/guardian, please have your parent/guardian email us
             at{" "}
