99c8666af5d05f7670e82d4885a3908707f5a711
Donavon West <github@donavon.com>
2020-01-13 15:00:28 -0500

    Clarify minor policy (#69)
    * fix grammar
    
    * add paragrpah on hosting company restrictions

--- src/pages/home/Minors.tsx
+++ src/pages/home/Minors.tsx
@@ -8,12 +8,16 @@ export default () => {
         <h2>A note for our younger attendees</h2>
         <TextGroup>
           <p>
-            If you're under the age of 18 you're welcome at our events, however
+            If you're under the age of 18, you're welcome at our events. However,
             we require that you have a chaperone 21 or older. If that person is
             not your parent/guardian, please have your parent/guardian email us
             at{" "}
             <a href="mailto:organizers@usereact.nyc">organizers@usereact.nyc</a>{" "}
             in advance with their name.
+          </p>
+          <p>
+            If the hosting company has age restrictions which override our policy,
+            we will note this on the meetup sign up page.
           </p>
         </TextGroup>
       </section>
